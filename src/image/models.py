be8666f1ee24cfb79799787dc16203eae96b0cc0
Arber Zela <zelaa@informatik.uni-freiburg.de>
2019-06-14 14:36:04 +0200

    separated download script for pretrained models

--- src/image/models.py
+++ src/image/models.py
@@ -1,21 +1,6 @@
 import torch
 import torchvision
 
-try:
-    import utils
-except ImportError:
-    # This file is used as a script
-    # TODO(Danny): Nicer solution to this
-    import sys
-    sys.path.append("src")
-    import utils
-
-
-def download_all(config):
-    resnet18_url = torchvision.models.resnet.model_urls["resnet18"]
-    torch.utils.model_zoo.load_url(resnet18_url, config.model_dir)
-
-
 models = {"resnet18": torchvision.models.resnet18()}
 
 
@@ -30,7 +15,3 @@ def get_parameters(model_type, config):
     else:
         raise  # TODO(Danny): Error message
 
-
-if __name__ == "__main__":
-    config = utils.Config("src/config.hjson")
-    download_all(config)
