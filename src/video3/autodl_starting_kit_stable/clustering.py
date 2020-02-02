0efdc65321d892d37d213673dacdf57cc6c48904
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2020-02-02 01:33:25 +0100

    k-means clustering of 18-dim meta-features and visualization

--- src/video3/autodl_starting_kit_stable/clustering.py
+++ src/video3/autodl_starting_kit_stable/clustering.py
@@ -7,19 +7,28 @@ from train_dataset_classifier import load_datasets_processed
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import minmax_scale
 
 
-def _plot_clusters(data, colors, cluster_labels, labels, ax_handle):
+def _plot_clusters(data, colors, cluster_labels, labels, ax_handle, annotate=False):
     for i, color in enumerate(colors):
         indices = np.where(cluster_labels == labels[i])
         X = data[indices]
         ax_handle.scatter(X[:, 0], X[:, 1], color=color, label=labels[i])
+        if annotate:
+            offset = np.random.uniform(-0.1, 0.1)
+            while np.isclose(offset, 0, rtol=1e-02):
+                offset = np.random.uniform(-0.1, 0.1)
+            # simply annotate the first sample
+            ax_handle.annotate(labels[i], (X[0, 0]+offset, X[0, 1]+offset), color=color)
     return ax_handle
 
 
 cfg = {}
+annotate = True
 meta_features_path = '/home/autodl/processed_datasets/1e4_meta'
-plot_name = ''
+#meta_features_path = '/home/autodl/processed_datasets/1e4_combined'
+
 cfg['proc_dataset_dir'] = meta_features_path
 cfg["datasets"] = ['Chucky', 'Decal', 'Hammer', 'Hmdb51', 'Katze', 'Kreatur', 'Munster', 'Pedro', 'SMv2', 'Ucf101',
                          'binary_alpha_digits', 'caltech101', 'caltech_birds2010', 'caltech_birds2011', 'cats_vs_dogs', 'cifar100',
@@ -39,8 +48,11 @@ X_labels = [ds[2] for ds in dataset_list]
 #X_test = [ds[1].dataset.numpy() for ds in dataset_list]
 #X_test = np.concatenate(X_test, axis=0)
 
+""" normalization (for now: only scale to [0,1] to preserve shape and outliers) """
+X_train = minmax_scale(X_train, axis=0)  # axis=0 -> scale each feature independently (not sample)
+
 """ fit k-means clustering """
-kmeans_estimator = KMeans(n_clusters=n_clusters, random_state=0).fit(X_train)
+kmeans_estimator = KMeans(n_clusters=n_clusters, random_state=0, n_jobs=4).fit(X_train)
 
 """ predict the cluster index for each sample """
 X_indices = kmeans_estimator.predict(X_train)
@@ -58,20 +70,20 @@ fig = plt.figure(figsize=(12, 12))
 colors = plt.cm.rainbow(np.linspace(0, 1, len(X_labels_unique)))
 
 ax1 = plt.subplot(211)
-ax1 = _plot_clusters(data=X_train_tsne_embedding, colors=colors, cluster_labels=X_labels_cluster_order, labels=X_labels_unique, ax_handle=ax1)
+ax1 = _plot_clusters(data=X_train_tsne_embedding, colors=colors, cluster_labels=X_labels_cluster_order, labels=X_labels_unique, ax_handle=ax1, annotate=annotate)
 ax1.legend(prop={'size': 7}, loc="upper right")
 ax1.title.set_text("t-SNE")
 
 ax2 = plt.subplot(212)
-ax2 = _plot_clusters(data=X_train_pca_embedding, colors=colors, cluster_labels=X_labels_cluster_order, labels=X_labels_unique, ax_handle=ax2)
+ax2 = _plot_clusters(data=X_train_pca_embedding, colors=colors, cluster_labels=X_labels_cluster_order, labels=X_labels_unique, ax_handle=ax2, annotate=annotate)
 ax2.title.set_text("PCA")
-ax2.set_xlim(right=20000)
-ax2.set_ylim(top=2000)
 ax2.legend(prop={'size': 7}, loc="upper right")
 
 plot_title = "Datasets clustered with k-means ({}-dim. meta-features, {} samples per dataset)".format(n_feature_dimensions, n_samples_to_use)
 fig.suptitle(plot_title, fontsize=14)
-plt.savefig(os.path.join(meta_features_path, "cluster_{}_samples.png".format(n_samples_to_use)))
+file_path = os.path.join(meta_features_path, "cluster_{}_samples.png".format(n_samples_to_use))
+plt.savefig(file_path)
+print("file saved to {}".format(file_path))
 plt.show()
 
 
