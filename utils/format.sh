5e317dba2d4001444f4905cbeaa8d4dcbf3659c7
Thomas <thomasnierhoff@web.de>
2020-02-05 16:13:50 +0100

    added documentation

--- utils/format.sh
+++
@@ -1,6 +0,0 @@
-if [ -f ".miniconda/bin/activate" ]; then
-    # Use local miniconda installation
-    source .miniconda/bin/activate
-fi
-conda activate autodl
-pre-commit run --all-files
--- utils/setup_models.sh
+++
@@ -1,7 +0,0 @@
-if [ -f ".miniconda/bin/activate" ]; then
-    # Use local miniconda installation
-    source .miniconda/bin/activate
-fi
-eval "$(conda shell.bash hook)"
-conda activate autodl
-python src/image/download_pretrained_models.py
