c85d04c4d6a447b5d91943eb813761c5270ec54f
Danny <stolld@cs.uni-freiburg.de>
2020-02-09 21:22:24 +0100

    add 22 train / 5 val split to datasets

--- src/available_datasets.py
+++ src/available_datasets.py
@@ -1,6 +1,26 @@
-available_datasets = [
-    "Chucky", "Decal", "Hammer", "Hmdb51", "Katze", "Kreatur", "Munster", "Pedro", "SMv2", "Ucf101",
-    "binary_alpha_digits", "caltech101", "caltech_birds2010", "caltech_birds2011", "cats_vs_dogs",
-    "cifar100", "cifar10", "coil100", "colorectal_histology", "deep_weeds", "emnist", "eurosat",
-    "fashion_mnist", "horses_or_humans", "kmnist", "mnist", "oxford_flowers102"
+train_datasets = [
+    "Chucky",
+    "Hammer",
+    "Hmdb51",
+    "Kreatur",
+    "Munster",
+    "Pedro",
+    "SMv2",
+    "Ucf101",
+    "binary_alpha_digits",
+    "caltech101",
+    "caltech_birds2010",
+    "caltech_birds2011",
+    "cats_vs_dogs",
+    "cifar100",
+    "cifar10",
+    "colorectal_histology",
+    "deep_weeds",
+    "emnist",
+    "eurosat",
+    "fashion_mnist",
+    "horses_or_humans",
+    "mnist",
 ]
+
+val_datasets = ["Decal", "Katze", "coil100", "kmnist", "oxford_flowers102"]
