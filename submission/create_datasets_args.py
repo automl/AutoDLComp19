7e3542e52a48eed818c5104811147af4dd79ffb5
ferreirafabio <fabioferreira@mailbox.org>
2020-02-18 00:37:46 +0100

    workers active/inactive are now specified in args files as well

--- submission/create_datasets_args.py
+++ submission/create_datasets_args.py
@@ -2,13 +2,16 @@ from pathlib import Path
 from src.available_datasets import all_datasets
 
 
-def construct_command(dataset):
-    return "--experiment_name {}".format(dataset)
+def construct_command(dataset, worker):
+    if worker:
+        return "--experiment_name {} --optimize_generalist --worker".format(dataset)
+    else:
+        return "--experiment_name {} --optimize_generalist".format(dataset)
 
 
 def generate_all_commands(commands_file, remove_datasets):
     commands = [
-        construct_command(dataset) for dataset in all_datasets if not dataset in remove_datasets
+        construct_command(dataset, worker) for dataset in all_datasets for worker in [True, False] if not dataset in remove_datasets
     ]
     commands_file.write_text("\n".join(commands))
 
@@ -17,7 +20,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--command_file_name", default="dataset_v1.args", help=" ")
+    parser.add_argument("--command_file_name", default="dataset_all_opt_v2.args", help=" ")
     args = parser.parse_args()
 
     remove_datasets = ["emnist"]
--- submission/meta_kakaobrain_optimized_all_datasets.sh
+++ submission/meta_kakaobrain_optimized_all_datasets.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p bosch_gpu-rtx2080
 #SBATCH -t 0-24:00
-#SBATCH -a 1-240
+#SBATCH -a 1-24
 
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task 1
@@ -14,13 +14,13 @@
 
 source ~/.miniconda/bin/activate autodl
 
-ARGS_FILE=submission/dataset_v1.args
+ARGS_FILE=submission/dataset_all_opt_v2.args
 TASK_SPECIFIC_ARGS=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $ARGS_FILE)
 
 python src/hpo/optimize.py \
     --nic_name eth0 \
     --job_id $SLURM_ARRAY_JOB_ID \
-    --experiment_group kakaobrain_optimized_per_dataset_test \
+    --experiment_group kakaobrain_optimized_all_datasets \
     --time_budget 1200 \
     --time_budget_approx 90 \
     $TASK_SPECIFIC_ARGS
