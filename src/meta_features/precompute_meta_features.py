d8a4d1608ddc7c42869dd8bfe96f4b5ec7a51bba
ferreirafabio <fabioferreira@mailbox.org>
2020-02-18 15:43:27 +0100

    implemented nn meta features to pandas and csv conversion

--- src/meta_features/precompute_meta_features.py
+++ src/meta_features/precompute_meta_features.py
@@ -1,8 +1,7 @@
 import yaml
-import os
+import random
 import pandas as pd
 import numpy as np
-import collections
 
 from src.available_datasets import all_datasets, train_datasets, val_datasets
 from pathlib import Path
@@ -58,67 +57,68 @@ def load_processed_datasets(dataset_path):
     return processed_datasets
 
 
-def precompute_nn_meta_features(dataset_path, normalize_features=False):
-    processed_datasets = load_processed_datasets(dataset_path=dataset_path)
+def precompute_nn_meta_features(dataset_path,
+                                output_path,
+                                dump_dataframe_csv=True,
+                                split_df=True,
+                                n_samples_to_use=100,
+                                file_name="meta_features"):
+
+    processed_datasets = load_processed_datasets(dataset_path=str(dataset_path))
+
     print("getting features ...")
     print("using data: {}".format(dataset_path))
 
-    train_feature_data = [ds[0].dataset.numpy()
-                          for ds in processed_datasets]
-    train_feature_data = np.concatenate(train_feature_data, axis=0)
+    # as before, we're for now only using the train ([0]) data
+    dataset_to_nn_meta_features = {}
+    for dataset in processed_datasets:
+        data = dataset[0].dataset.numpy()
+        sample_indices = np.random.choice(data.shape[0], n_samples_to_use, replace=False)
+        data_sampled_flattened = np.concatenate(data[sample_indices, :], axis=0)
+        dataset_to_nn_meta_features[dataset[2]] = data_sampled_flattened
 
-    test_feature_data = [ds[1].dataset.numpy()
-                         for ds in processed_datasets]
-    test_feature_data = np.concatenate(test_feature_data, axis=0)
-
-    # TODO
-    if normalize_features:
-        raise NotImplementedError
-
-    return train_feature_data, test_feature_data, processed_datasets
+    df = pd.DataFrame(columns=[np.arange(data_sampled_flattened.shape[0])])
+    for i, dataset_name in enumerate(sorted(list(dataset_to_nn_meta_features.keys()))):
+        df.loc[dataset_name] = dataset_to_nn_meta_features[dataset_name]
 
 
-def export_nn_meta_features(dataset_path, path_export_dir, normalize_features=False, use_train_data=True):
-    train_feature_data, test_feature_data, processed_datasets = \
-        precompute_nn_meta_features(dataset_path=dataset_path, normalize_features=normalize_features)
+    if dump_dataframe_csv:
+        dump_meta_features_df_and_csv(
+            meta_features=df,
+            output_path=output_path,
+            split_df=split_df,
+            file_name=file_name
+        )
 
-    if use_train_data:
-        features = train_feature_data
-        csv_file_name = "features_train_data.csv"
+    return df
+
+
+def dump_meta_features_df_and_csv(meta_features,
+                                  output_path,
+                                  split_df=True,
+                                  file_name="meta_features"):
+
+    if not isinstance(meta_features, pd.DataFrame):
+        df = convert_metadata_to_df(meta_features)
     else:
-        features = test_feature_data
-        csv_file_name = "features_test_data.csv"
+        df = meta_features
 
-    feature_dimension = features.shape[1]
-    instance_labels = [ds[2] for ds in processed_datasets]
-
-    feature_data_file_path = os.path.join(path_export_dir, csv_file_name)
-
-    df = pd.DataFrame(columns=[np.arange(feature_dimension)])
-    for i, instance_label in enumerate(instance_labels):
-        df.loc[instance_label] = features[i]
-
-    df.to_csv(feature_data_file_path, float_format="%.5f")
-    print("feature data dumped to: {}".format(feature_data_file_path))
-
-
-def dump_meta_features_df_and_csv(meta_features, output_path, split_df=True):
-    df = convert_metadata_to_df(meta_features)
     if split_df:
         df_train = df.loc[df.index.isin(train_datasets)]
         df_valid = df.loc[df.index.isin(val_datasets)]
 
-        df_train.to_csv(output_path.parent / "meta_features_train.csv")
-        df_train.to_pickle(output_path.parent / "meta_features_train.pkl")
+        df_train.to_csv(output_path / Path(file_name + "_train.csv"))
+        df_train.to_pickle(output_path / Path(file_name + "_train.pkl"))
 
-        df_valid.to_csv(output_path.parent / "meta_features_valid.csv")
-        df_valid.to_pickle(output_path.parent / "meta_features_valid.pkl")
+        df_valid.to_csv(output_path / Path(file_name + "_valid.csv"))
+        df_valid.to_pickle(output_path / Path(file_name + "_valid.pkl"))
 
     else:
+        output_path = output_path / file_name
         df.to_csv(output_path.with_suffix(".csv"))
         df.to_pickle(output_path.with_suffix(".pkl"))
 
-    print("meta features data dumped to: {}".format(output_path.parent))
+    print("meta features data dumped to: {}".format(output_path))
 
 
 def get_meta_features_from_dataset(dataset_path):
@@ -131,7 +131,26 @@ def get_meta_features_from_dataset(dataset_path):
     return parse_meta_features(meta_data)
 
 
-def precompute_meta_features(dataset_path, output_path, dump_dataframe_csv=True, split_df=True):
+def get_nn_meta_features_from_dataset(dataset_path):
+    if isinstance(dataset_path, Path):
+        dataset_path = str(dataset_path)
+
+    processed_datasets = load_processed_datasets(dataset_path=dataset_path)
+    print("getting features ...")
+    print("using data: {}".format(dataset_path))
+
+    train_feature_data = [ds[0].dataset.numpy() for ds in processed_datasets]
+    train_feature_data = np.concatenate(train_feature_data, axis=0)
+    return train_feature_data
+
+
+
+def precompute_meta_features(dataset_path,
+                             output_path,
+                             dump_dataframe_csv=True,
+                             split_df=True,
+                             file_name="meta_features"):
+
     dataset_to_meta_features = {
         dataset.name: get_meta_features_from_dataset(dataset)
         for dataset in dataset_path.iterdir() if dataset.name in all_datasets
@@ -141,10 +160,13 @@ def precompute_meta_features(dataset_path, output_path, dump_dataframe_csv=True,
         dump_meta_features_df_and_csv(
             meta_features=dataset_to_meta_features,
             output_path=output_path,
-            split_df=split_df
+            split_df=split_df,
+            file_name=file_name
         )
 
-    with output_path.open("w") as out_stream:
+    output_path_yaml = output_path / file_name
+    output_path_yaml.with_suffix(".yaml")
+    with output_path_yaml.open("w") as out_stream:
         yaml.dump(dataset_to_meta_features, out_stream)
 
 
@@ -152,19 +174,48 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--seed", type=int, default=2, help="random seed")
+    parser.add_argument(
+        "--dataset_path",
+        default="/data/aad/image_datasets/processed_datasets/1e4_combined",
+        type=Path,
+        help=" "
+    )
 
     parser.add_argument(
-        "--dataset_path", default="/data/aad/image_datasets/all_symlinks/", type=Path, help=" "
-    )
-    parser.add_argument(
-        "--output_path", default="src/meta_features/meta_features.yaml", type=Path, help=" "
+        "--output_path", default="src/meta_features/nn", type=Path, help=" "
     )
 
     args = parser.parse_args()
 
-    precompute_meta_features(
-         args.dataset_path,
-         args.output_path,
-         dump_dataframe_csv=True,
-         split_df=False
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+
+    precompute_nn_meta_features(
+            args.dataset_path,
+            args.output_path,
+            dump_dataframe_csv=True,
+            split_df=True
     )
+
+
+    """ example (non-nn) meta features """
+    # parser.add_argument(
+    #     "--dataset_path",
+    #     default="/data/aad/image_datasets/all_symlinks/",
+    #     type=Path,
+    #     help=" "
+    # )
+
+    # parser.add_argument(
+    #     "--output_path", default="src/meta_features/non-nn", type=Path, help=" "
+    # )
+
+    # args = parser.parse_args()
+
+    # precompute_meta_features(
+    #      args.dataset_path,
+    #      args.output_path,
+    #      dump_dataframe_csv=True,
+    #      split_df=False
+    # )
