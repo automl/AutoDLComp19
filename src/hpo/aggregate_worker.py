cf40e63ad1a4368e2c56e5d199fde56845226141
Arber Zela <zelaa@cs.uni-freiburg.de>
2020-02-27 12:53:19 +0100

    checkout to master

--- src/hpo/aggregate_worker.py
+++ src/hpo/aggregate_worker.py
@@ -1,8 +1,11 @@
+import random
 from pathlib import Path
 
 import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
 import numpy as np
+import tensorflow as tf
+import torch
 import yaml
 from hpbandster.core.worker import Worker
 from src.available_datasets import train_datasets
@@ -42,32 +45,32 @@ def get_configspace():
     # Dataset sizes
     cv_valid_ratio = CSH.UniformFloatHyperparameter("cv_valid_ratio", lower=0.05, upper=0.2)
     max_valid_count = CSH.UniformIntegerHyperparameter("max_valid_count", lower=128, upper=512,
-        log=True)
+                                                       log=True)
     max_size = CSH.UniformIntegerHyperparameter("log2_max_size", lower=5, upper=7)
     max_times = CSH.UniformIntegerHyperparameter("max_times", lower=4, upper=10)
     train_info_sample = CSH.UniformIntegerHyperparameter("train_info_sample", lower=128, upper=512,
-        log=True)
+                                                         log=True)
     enough_count_video = CSH.UniformIntegerHyperparameter("enough_count_video", lower=100,
-        upper=10000, log=True)
+                                                          upper=10000, log=True)
     enough_count_image = CSH.UniformIntegerHyperparameter("enough_count_image", lower=1000,
-        upper=100000, log=True)
+                                                          upper=100000, log=True)
 
     # Report intervalls
     steps_per_epoch = CSH.UniformIntegerHyperparameter("steps_per_epoch", lower=5, upper=250,
-        log=True)
+                                                       log=True)
     early_epoch = CSH.UniformIntegerHyperparameter("early_epoch", lower=1, upper=3)
     skip_valid_score_threshold = CSH.UniformFloatHyperparameter("skip_valid_score_threshold",
-        lower=0.7, upper=0.95)
+                                                                lower=0.7, upper=0.95)
     test_after_at_least_seconds = CSH.UniformIntegerHyperparameter("test_after_at_least_seconds",
-        lower=1, upper=3)
+                                                                   lower=1, upper=3)
     test_after_at_least_seconds_max = CSH.UniformIntegerHyperparameter(
         "test_after_at_least_seconds_max", lower=60, upper=120)
     test_after_at_least_seconds_step = CSH.UniformIntegerHyperparameter(
         "test_after_at_least_seconds_step", lower=2, upper=10)
     threshold_valid_score_diff = CSH.UniformFloatHyperparameter("threshold_valid_score_diff",
-        lower=0.0001, upper=0.01, log=True)
+                                                                lower=0.0001, upper=0.01, log=True)
     max_inner_loop_ratio = CSH.UniformFloatHyperparameter("max_inner_loop_ratio", lower=0.1,
-        upper=0.3)
+                                                          upper=0.3)
 
     # Optimization
     batch_size = CSH.UniformIntegerHyperparameter("batch_size", lower=16, upper=64, log=True)
@@ -147,17 +150,22 @@ class AggregateWorker(Worker):
         config_experiment_path = Path(self._working_directory, config_id_formated, str(budget))
 
         model_config = construct_model_config(config, default_config=self._default_config)
-        score_results_tuples = [
-            _run_on_dataset(
-                dataset,
-                config_experiment_path / dataset,
-                model_config,
-                dataset_dir=self._dataset_dir,
-                n_repeat=self.n_repeat,
-                time_budget=budget,
-                time_budget_approx=self.time_budget_approx
-            ) for dataset in train_datasets
-        ]
+
+        score_results_tuples = []
+        for dataset in train_datasets:
+            try:
+                score = _run_on_dataset(
+                    dataset,
+                    config_experiment_path / dataset,
+                    model_config,
+                    dataset_dir=self._dataset_dir,
+                    n_repeat=self.n_repeat,
+                    time_budget=self.time_budget,
+                    time_budget_approx=self.time_budget_approx
+                )
+            except RuntimeError:
+                score = (None, 0)
+            score_results_tuples.append(score)
 
         # just get the repetition means for optimization
         repetition_scores_mean_per_dataset = np.array(score_results_tuples)[:, 1]
@@ -181,24 +189,32 @@ class AggregateWorker(Worker):
 
 
 if __name__ == "__main__":
-    worker = AggregateWorker(working_directory='experiments/test/test_aggregate_worker',
-                             n_repeat=1,
-                             run_id='0',
-                             time_budget=60,
-                             time_budget_approx=1200
-                             )
+    random.seed(2)
+    np.random.seed(2)
+    torch.manual_seed(2)
+    torch.cuda.manual_seed_all(2)
+    tf.set_random_seed(2)
+
+    worker = AggregateWorker(
+        working_directory='experiments/test/test_aggregate_worker',
+        n_repeat=1,
+        run_id='0',
+        time_budget=1200,
+        time_budget_approx=60
+    )
 
     # worker = SingleWorker(
     #     working_directory='experiments/test/test_aggregate_worker',
     #     n_repeat=1,
     #     run_id='0',
     #     dataset="emnist",
-    #     time_budget=60,
-    #     time_budget_approx=1200
+    #     time_budget=1200,
+    #     time_budget_approx=60
     # )
     cs = get_configspace()
 
     config = cs.sample_configuration().get_dictionary()
     print(config)
+
     res = worker.compute(config=config, config_id=(0, 0, 0), budget=1)
     print(res)
