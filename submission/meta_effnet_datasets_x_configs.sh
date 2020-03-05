6a1315657a6912271a2c4419f730d21920703ef5
Arber Zela <zelaa@cs.uni-freiburg.de>
2020-03-05 12:50:34 +0100

    added performance matrix evaluation script

--- submission/meta_effnet_datasets_x_configs.sh
+++ submission/meta_effnet_datasets_x_configs.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p bosch_gpu-rtx2080
 #SBATCH -t 0-01:00
-#SBATCH -a 1-5
+#SBATCH -a 1-2069
 
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task 1
@@ -10,9 +10,9 @@
 #SBATCH -o experiments/effnet_datasets_x_configs_%A-%a.%x.out
 #SBATCH -e experiments/effnet_datasets_x_configs_%A-%a.%x.err
 
-#SBATCH --job-name def_cfg
+#SBATCH --job-name effnet_def_cfg
 
-source ~/.miniconda/bin/activate autodl
+source activate autodl
 
 ARGS_FILE=submission/effNet_dataset_x_configs.args
 TASK_SPECIFIC_ARGS=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $ARGS_FILE)
