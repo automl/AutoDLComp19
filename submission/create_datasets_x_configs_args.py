bc90faff0fc1d151be46994c8a9ce07d94daa831
ferreirafabio <fabioferreira@mailbox.org>
2020-03-07 15:27:19 +0100

    default config file bugfix

--- submission/create_datasets_x_configs_args.py
+++ submission/create_datasets_x_configs_args.py
@@ -15,8 +15,9 @@ def construct_command(config, dataset, base_datasets_dir, repeat, configs_path):
 
 
 def generate_all_commands(configs_path, commands_file, n_repeats):
-    with Path(configs_path, "default.yaml").open() as in_stream:
+    with Path(configs_path.parent, "default.yaml").open() as in_stream:
         config = yaml.safe_load(in_stream)
+        print("using {} as default config file".format(str(configs_path.parent) + "/default.yaml"))
     base_datasets_dir = config["cluster_datasets_dir"]
 
     all_configs = [config_path.name for config_path in configs_path.glob("*")]
